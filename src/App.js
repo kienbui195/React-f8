4d17fb78dfb1c6c5d1c3352f03409acd47a578e1
KienBui <kaybi9x@gmail.com>
2022-12-13 11:10:00 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,56 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [status, setStatus] = useState({ display: 'none' });
+  const type = ['posts', 'users', 'photos'];
+  const [btn, setBtn] = useState('posts');
+  const [data, setData] = useState([]);
+  const [gotoTop, setGotoTop] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+
+
+  const handleChange = () => {
+    status.display === 'none' ? setStatus({ display: 'block' }) : setStatus({ display: 'none' });
+  }
+
+  const callApi = async (btn) => {
+    return await axios.get(`https://jsonplaceholder.typicode.com/${btn}`)
+  }
+
+  useEffect(() => {
+    callApi(btn)
+      .then(res => {
+        setData(res.data);
+      })
+      .catch(err => console.error(err))
+  }, [btn])
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 200) {
+        setGotoTop(true);
+      } else {
+        setGotoTop(false);
+      }
+    }
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, [])
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    }
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+
+  },[])
+
+
+
+  console.log(gotoTop);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className='container' id='top'>
+      <div className='content'>
+        <button onClick={handleChange}>Click me!</button>
+        <h5>width: { width}</h5>
+        <div style={status}>
+          {type.map(item => (
+            <button
+              className='btn'
+              key={item}
+              style={btn === item ? {
+                backgroundColor: 'green',
+                color: 'white'
+              } : {}}
+              onClick={() => setBtn(item)}
+            >
+              {item}
+            </button>
+          ))}
+          <div>
+            <ul>
+              {data.map((item) => (
+                <li key={item.id}>{item.title || item.name}</li>
+              ))}
+              
+            </ul>
+            
+          </div>
+          {gotoTop && (
+                <a className="gotoTop" href='#'>Go Top</a>
+              )}
+        </div>
+      </div>
+
+
+
+
     </div>
   );
 }
